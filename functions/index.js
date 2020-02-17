3d49eda3f8458883a8e4c9a5664f480ae5fc921b
Matt Porter <matthew.js.porter@gmail.com>
2020-02-17 14:59:55 -0500

    Setting up firebase as part of lab2 to make deploy changes from the firebase cli

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,16 @@
+// Copyright 2018, Google, Inc.
+// Licensed under the Apache License, Version 2.0 (the 'License');
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an 'AS IS' BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 'use strict';
 
 // Import the Dialogflow module from the Actions on Google client library.
@@ -12,9 +25,9 @@ const app = dialogflow({debug: true});
 // Handle the Dialogflow intent named 'favorite color'.
 // The intent collects a parameter named 'color'.
 app.intent('favorite color', (conv, {color}) => {
-    const luckyNumber = color.length;
-    // Respond with the user's lucky number and end the conversation.
-    conv.close('Your lucky number is ' + luckyNumber);
+  const luckyNumber = color.length;
+  // Respond with the user's lucky number and end the conversation.
+  conv.close('Your lucky number is ' + luckyNumber);
 });
 
 // Set the DialogflowApp object to handle the HTTPS POST request.
