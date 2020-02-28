811751853891cc4e7314dcbb1aadad06d5005fa9
Matt Porter <matthew.js.porter@gmail.com>
2020-02-28 13:19:39 -0500

    adding storage for username so google will remember the name between sessions

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 // Copyright 2018, Google, Inc.
 // Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
@@ -31,10 +32,10 @@ const app = dialogflow({debug: true});
 app.intent('favorite color', (conv, {color}) => {
   const luckyNumber = color.length;
   const audioSound = 'https://actions.google.com/sounds/v1/cartoon/clang_and_wobble.ogg';
-  if (conv.data.userName) {
+  if (conv.user.storage.userName) {
     // If we collected user name previously, address them by name and use SSML
     // to embed an audio snippet in the response.
-    conv.ask(`<speak>${conv.data.userName}, your lucky number is ` +
+    conv.ask(`<speak>${conv.user.storage.userName}, your lucky number is ` +
       `${luckyNumber}.<audio src="${audioSound}"></audio> ` +
       `Would you like to hear some fake colors?</speak>`);
     conv.ask(new Suggestions('Yes', 'No'));
@@ -48,11 +49,17 @@ app.intent('favorite color', (conv, {color}) => {
 
 // Handle the Dialogflow intent named 'Default Welcome Intent'.
 app.intent('Default Welcome Intent', (conv) => {
-  conv.ask(new Permission({
-    context: 'Hi there, to get to know you better',
-    permissions: 'NAME',
-  }));
-});
+  const name = conv.user.storage.userName;
+  if (!name) {
+    // Asks the user's permission to know their name, for personalization.
+    conv.ask(new Permission({
+      context: 'Hi there, to get to know you better',
+      permissions: 'NAME',
+    }));
+  } else {
+    conv.ask(`Hi again, ${name}. What's your favorite color?`);
+  }
+ });
 
 // Handle the Dialogflow intent named 'actions_intent_PERMISSION'. If user
 // agreed to PERMISSION prompt, then boolean value 'permissionGranted' is true.
@@ -61,8 +68,8 @@ app.intent('actions_intent_PERMISSION', (conv, params, permissionGranted) => {
     conv.ask(`Ok, no worries. What's your favorite color?`);
     conv.ask(new Suggestions('Blue', 'Red', 'Green'));
   } else {
-    conv.data.userName = conv.user.name.display;
-    conv.ask(`Thanks, ${conv.data.userName}. What's your favorite color?`);
+    conv.user.storage.userName = conv.user.name.display;
+    conv.ask(`Thanks, ${conv.user.storage.userName}. What's your favorite color?`);
     conv.ask(new Suggestions('Blue', 'Red', 'Green'));
   }
 });
